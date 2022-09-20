810f733d680d33ad6d45de8c596d1941411c5b27
GuilhermeFerrariNeto <guilhermeferrarint@hotmail.com>
2022-09-20 12:01:14 -0300

    primeiro código

---
+++ sistema de senha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void manin() {
+    int vsenha, vvalor;
+
+    printf("crie uma senha:");
+    scanf("%i", &vsenha);
+
+Whilhe(vesenha != vvalor) {
+    printf("digite sua senha:");
+    printf("senha incorreta, digite novamente:");
+    scanf("%i" &vvalor);
+}
+printf("acesso liberado!!!");
+}
